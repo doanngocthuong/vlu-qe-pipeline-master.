af49d850d05708e42649e7fdc9d24ce55a4f5926
doanngocthuong <doanngocthuong06@gmail.com>
2026-09-21 13:38:53 +0700

    auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,20 @@
+function login(username, password) {
+    // Kiểm tra dữ liệu rỗng
+    if (!username || !password) {
+        return false;
+    }
 
+    // Giả lập tài khoản bị khóa
+    if (username === "locked") {
+        return false;
+    }
+
+    // Tài khoản hợp lệ
+    if (username === "admin" && password === "123") {
+        return true;
+    }
+
+    return false;
+}
+
+module.exports = { login };
